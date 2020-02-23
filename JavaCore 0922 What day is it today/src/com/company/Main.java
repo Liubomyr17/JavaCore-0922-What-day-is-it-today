ebab163bfeafecec238e190c68d2c4724b97bbcf
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-23 09:29:18 +0200

    Add files via upload

---
+++ JavaCore 0922 What day is it today/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+/*
+
+0923 Vowels and consonants
+Write a program that enters a line of text from the keyboard.
+The program should display two lines:
+1. The first line contains only the vowels from the entered line.
+2. second - only consonants and punctuation marks from the entered line.
+Letters connect with a space, each line should end with a space.
+
+Input Example:
+Mom washed the frame.
+Output Example:
+a y a a u
+M m m l r m
+Requirements:
+1. The program should read data from the keyboard.
+2. The program should output two lines.
+3. The first line should contain only vowels from the entered line, separated by a space.
+4. The second line should contain only consonants and punctuation marks from the entered line, separated by a space.
+5. Each line must end with a space.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+
+        public static void main(String[] args) throws Exception {
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        }
+    }
+
+
+
+
+
+
